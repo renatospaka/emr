cd85a40e5daa6a7af4f1fff6e6eee58fabdec0a4
Renato Spakauskas <renatospaka@gmail.com>
2022-05-29 00:42:08 -0300

    feat: :sparkles: find family by id
    find by id and change name features on the in memory structure

--- domain/repository/family/family_test.go
+++ domain/repository/family/family_test.go
@@ -1,8 +1,9 @@
 package familyRepository_test
 
 import (
-	// "errors"
 	"testing"
+
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/require"
 
 	"github.com/renatospaka/emr/domain/entity/family"
@@ -11,9 +12,58 @@ import (
 
 func TestFamily_Create(t *testing.T) {
 	var repo repoFamily.FamilyRepositoryInMemory 
-	fam := family.NewFamily("Essa Família")
+	fam := family.NewFamily("Dino da Silva")
 
 	err := repo.Create(fam)
 	require.Nil(t, err)
-	require.Equal(t, fam.Surname, "Essa Família")
+	require.Equal(t, fam.Surname, "Dino da Silva")
+}
+
+func TestFamily_FindById(t *testing.T) {
+	var repo repoFamily.FamilyRepositoryInMemory 
+	fam := family.NewFamily("Dino da Silva")
+	_ = repo.Create(fam)
+
+	id := fam.ID
+	find, err := repo.FindById(id)
+	require.Nil(t, err)
+	require.Equal(t, find.ID, fam.ID)
+}
+
+func TestFamily_FindById_NotFound(t *testing.T) {
+	var repo repoFamily.FamilyRepositoryInMemory 
+	fam := family.NewFamily("Dino da Silva")
+	_ = repo.Create(fam)
+
+	id := uuid.NewV4()
+	find, err := repo.FindById(id)
+	require.NotNil(t, err)
+	require.Error(t, err, "família não encontrada")
+	require.Equal(t, find, &family.Family{})
+}
+
+func TestFamily_ChangeName(t *testing.T) {
+	var repo repoFamily.FamilyRepositoryInMemory 
+	fam := family.NewFamily("Dino da Silva")
+	_ = repo.Create(fam)
+
+	id := fam.ID
+	updSurname := "Dino da Silva Sauro"
+	upd, err := repo.ChangeName(id, updSurname)
+	require.Nil(t, err)
+	require.Equal(t, upd.Surname, "Dino da Silva Sauro")
+}
+
+func TestFamily_ChangeName_NotFound(t *testing.T) {
+	var repo repoFamily.FamilyRepositoryInMemory 
+	fam := family.NewFamily("Dino da Silva")
+	_ = repo.Create(fam)
+
+	id := uuid.NewV4()
+	updSurname := "Dino da Silva Sauro"
+	upd, err := repo.ChangeName(id, updSurname)
+	require.NotNil(t, err)
+	require.NotEqual(t, upd.Surname, "Dino da Silva Sauro")
+	require.Error(t, err, "família não encontrada")
+	require.Equal(t, upd, &family.Family{})
 }
--- infrastructure/repository/family/familyInMemory.go
+++ infrastructure/repository/family/familyInMemory.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 
 	"github.com/renatospaka/emr/domain/entity/family"
+	uuid "github.com/satori/go.uuid"
 )
 
 
@@ -11,10 +12,36 @@ type FamilyRepositoryInMemory struct {
 	family []family.Family
 }
 
+// Creates a new family structure
+// or returns an error
 func (f *FamilyRepositoryInMemory) Create(family *family.Family) error {
 	f.family = append(f.family, *family)
 	if len(f.family) > 0 {
 		return nil
 	}
 	return errors.New("ocorreu um erro na gravação da nova família")
+}
+
+// Find a family by its ID
+// or returns an error
+func (f *FamilyRepositoryInMemory) FindById(id uuid.UUID) (*family.Family, error) {
+	for x, fam := range f.family {
+		if fam.ID == id {
+			return &f.family[x], nil
+		}
+	}
+	return &family.Family{}, errors.New("família não encontrada")
+}
+
+
+// Allow user to change or update the surname of a specific family
+// or returns an error
+func (f *FamilyRepositoryInMemory) ChangeName(id uuid.UUID, newSurname string) (*family.Family, error) {
+	fam, err := f.FindById(id)
+	if err != nil {
+		return &family.Family{}, errors.New("família não encontrada")
+	}
+
+	fam.Surname = newSurname
+	return fam, nil
 }
