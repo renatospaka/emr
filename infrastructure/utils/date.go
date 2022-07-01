269f03e40891aaa7902da32c271c8a6e95996f7e
Renato Spakauskas <renatospaka@gmail.com>
2022-07-01 15:13:59 -0300

    feat: :sparkles: AgeBetweenDates
    calculate elapsed time (in seconds) from two given dates and returns formated in months and years

--- infrastructure/utils/date.go
+++ infrastructure/utils/date.go
@@ -5,6 +5,7 @@ import (
 )
 
 // Calculate elapsed time (in seconds) from a given date
+// (today - day of birth)
 // and returns formated in months and years
 func AgeFromToday(dateFrom time.Time) (years int64, months int64) {
 	if dateFrom.IsZero() {
@@ -13,6 +14,20 @@ func AgeFromToday(dateFrom time.Time) (years int64, months int64) {
 
 	today := time.Now()
 	duration := today.Sub(dateFrom)
+	years = int64(duration.Hours() / 24 / 365)
+	months = int64(duration.Hours() / 24 / 30)
+	return years, months
+}
+
+// Calculate elapsed time (in seconds) from two given dates,
+// (recent date - date)
+// and returns formated in months and years
+func AgeBetweenDates(recentDate time.Time, date time.Time) (years int64, months int64) {
+	if recentDate.IsZero() || date.IsZero() {
+		return 0, 0
+	}
+
+	duration := recentDate.Sub(date)
 	years = int64(duration.Hours() / 24 / 365)
 	months = int64(duration.Hours() / 24 / 30)
 	return years, months
