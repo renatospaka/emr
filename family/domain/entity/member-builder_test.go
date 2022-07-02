1272e85d98a270decf728b75fb1c5509d15a4def
Renato Spakauskas <renatospaka@gmail.com>
2022-07-01 21:03:08 -0300

    feat: :sparkles: member builder
    a constructor (builder) to create a new member

---
+++ family/domain/entity/member-builder_test.go
@@ -0,0 +1,43 @@
+package family_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	family "github.com/renatospaka/emr/family/domain/entity"
+)
+
+var testMemberBuilder = &family.MemberBuilder{}
+
+func init() {
+	testMemberBuilder = family.NewMemberBuilder()
+}
+
+func TestMemberBuilder_Build(t *testing.T) {
+	member := testMemberBuilder.
+		WithFullName("Name", "Middle", "Last").
+		WithBirthDate(dobAdult).
+		WithGender(family.Male).
+		WithNickname("Nick").
+		Build()
+
+	require.IsTypef(t, &family.Member{}, member, "não é do tipo *Member{}")
+	require.True(t, member.IsValid())
+	require.Empty(t, member.Err())
+}
+
+func TestMemberBuilder_Build_Invalid(t *testing.T) {
+	member := testMemberBuilder.
+		WithFullName("Name", "Middle", "Last").
+		WithBirthDate(time.Time{}).
+		WithGender(family.Male).
+		WithNickname("Nick").
+		Build()
+
+	require.IsTypef(t, &family.Member{}, member, family.ErrMemberError.Error())
+	require.False(t, member.IsValid())
+	require.NotEmpty(t, member.Err())
+	require.Len(t, member.ErrToArray(), 5)
+}
