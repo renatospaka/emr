3da3228c4039825e37215ffc813ae4efeb0e37ee
Renato Spakauskas <renatospaka@gmail.com>
2022-07-14 23:11:06 -0300

    test: :white_check_mark: adding new unit tests
    increase unit test coverage for adding a new member to a family

--- family/domain/entity/family-member_test.go
+++ family/domain/entity/family-member_test.go
@@ -12,16 +12,7 @@ var (
 )
 
 func init() {
-	// testMemberBuilder = family.NewMemberBuilder()
 	testFamilyMemberBuilder = family.NewFamilyMemberBuilder()
-
-	// testMember = testMemberBuilder.
-	// 	WithFullName("Name2", "Ordinary", "Last2").
-	// 	WithBirthDate(dobAdult).
-	// 	WithGender(family.Female).
-	// 	WithNickname("Nick2").
-	// 	Build()
-
 }
 
 func TestFamilyMember_IsValid_HOF(t *testing.T) {
@@ -36,13 +27,10 @@ func TestFamilyMember_IsValid_HOF(t *testing.T) {
 		AsHOF(testMemberHOF).
 		Build()
 
-
-	require.True(t, famMember.IsHeadOfFamily())
-	require.True(t, testMemberHOF.IsAdult())
 	require.EqualValues(t, family.Self, famMember.RelationType())
+	require.True(t, famMember.IsHeadOfFamily())
 	require.True(t, famMember.IsValid())
 	require.Empty(t, famMember.Err())
-	// require.Len(t, famMember.ErrToArray(), 3)
 }
 
 func TestFamilyMember_Inalid_HOF_Empty(t *testing.T) {
@@ -52,5 +40,41 @@ func TestFamilyMember_Inalid_HOF_Empty(t *testing.T) {
 
 	require.False(t, famMember.IsValid())
 	require.NotEmpty(t, famMember.Err())
-	require.Len(t, famMember.ErrToArray(), 3)
+	require.Len(t, famMember.ErrToArray(), 2)
+}
+
+func TestFamilyMember_Inalid_HOF_Invalid_Age(t *testing.T) {
+	testMemberHOF = testMemberBuilder.
+		WithFullName("Name", "Invalid Age", "Last").
+		WithBirthDate(dobTeen).
+		WithGender(family.Male).
+		WithNickname("Teen").
+		Build()
+	
+	famMember := testFamilyMemberBuilder. 
+		AsHOF(testMemberHOF).
+		Build()
+
+	require.False(t, famMember.IsValid())
+	require.NotEmpty(t, famMember.Err())
+	require.Len(t, famMember.ErrToArray(), 1)
+}
+
+func TestFamilyMember_IsValid_Ordinary(t *testing.T) {
+	testMemberOrdinary := testMemberBuilder.
+		WithFullName("Name", "Ordinary", "Last").
+		WithBirthDate(dobAdult).
+		WithGender(family.Male).
+		WithNickname("Nick").
+		Build()
+	
+	famMember := testFamilyMemberBuilder. 
+		AsOrdinary(testMemberOrdinary).
+		RelatedAs(family.Father).
+		Build()
+
+	require.EqualValues(t, family.Father, famMember.RelationType())
+	require.False(t, famMember.IsHeadOfFamily())
+	require.True(t, famMember.IsValid())
+	require.Empty(t, famMember.Err())
 }
